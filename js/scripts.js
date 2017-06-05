5fad2ea88f5d92cb7c02ce24e7da98fd0007a9b8
Calla Rudolph and Larry Taylor <larry@>
2017-06-05 11:57:26 -0700

    reconfigure js forEach with assigned ids in HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 $(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-    alert("I told you, THIS IS A HEADER!")
-  });
+  var elements = ["h1", "p", "p2", "img"];
 
-  $("p").click(function() {
-    alert("This is a paragraph.");
-  });
 
-  $("img").click(function() {
-    alert("This is an image.");
+    elements.forEach(function(element) {
+      $("#" + element).click(function() {
+      alert("This is a " + element)
+    });
   });
+  //
+  // $("h1").click(function() {
+  //   alert("This is a header.");
+  //   alert("I told you, THIS IS A HEADER!")
+  // });
+  //
+  // $("p").click(function() {
+  //   alert("This is a paragraph.");
+  // });
+  //
+  // $("img").click(function() {
+  //   alert("This is an image.");
+  // });
 });
